c83a2906196b5b57ca2795e6c1786a45feb4160e
Samuel Bunga <sambunga094@gmail.com>
2021-08-16 12:12:54 -0400

    Fixed bugs and refactored the code

--- gsea/make_rnk.py
+++ gsea/make_rnk.py
@@ -1,44 +1,22 @@
 import os
 import re
+import gseapy
 import logging
 import pandas as pd
 
 
 logger = logging.getLogger()
 
-'''
-def make_rnk_files(files, DB):
-    for f in files:
-        file = pd.read_csv(os.path.join(WD, 'DEG_CLUSTER/', f),
-                           header=0, index_col=0)
-        genes, p_adj = [], []
-        for r, c in file.iterrows():
-            try:
-                hgnc_symbol = mgi_to_hgnc_name([r])
-                if hgnc_symbol != None:
-                    genes.append(hgnc_symbol)
-                    p_adj.append(c[4])
-            except:
-                continue
 
-        rnk_file = pd.DataFrame({'names': genes,
-                                 'p_adj': p_adj})
-        f_name = str(re.search('[a-zA-Z0-9_]+', f).group())
-        rnk_file.to_csv(os.path.join(WD, 'rnk', f_name + '.rnk'),
-                        index=False,
-                        sep='\t',
-                        header=False)
-        # Run GSEA
-        try:
-            os.mkdir(os.path.join(WD, 'outputs', DB, f_name), exist_ok=True)
-        except:
-            pass
-        gseapy.prerank(os.path.join(WD, 'rnk', f_name + '.rnk'),
-                       os.path.join(WD, 'Database/c2.cp.kegg.v7.2.symbols.gmt'),
-                       os.path.join(WD, 'outputs', DB, f_name)
-'''
+def run_gsea(indir, outdir, fname, db):
+    if not os.path.isdir(os.path.join(outdir, 'gsea_out')):
+        os.mkdir(os.path.join(outdir, 'gsea_out'))
+    gseapy.prerank(os.path.join(outdir, 'rnk', fname + '.rnk'),
+                   os.path.realpath(db),
+                   os.path.join(outdir, 'gsea_out', fname))
 
-def make_rnk(genes, p_adj, f, outdir):
+
+def make_rnk(genes, p_adj, f, indir, outdir, db):
     logger.info("Creating rank file for {}".format(os.path.basename(f)))
     # create rank dir
     if not os.path.isdir(os.path.join(outdir, 'rnk')):
@@ -54,11 +32,15 @@ def make_rnk(genes, p_adj, f, outdir):
                     index=False,
                     sep='\t',
                     header=False)
+
+    logger.info('Starting gsea')
+    run_gsea(indir, outdir, fname, db)
     logger.info("Success!")
 
 
-def format_deseq2(indir, outdir):
-    files = [f for f in os.listdir(os.path.realpath(indir)) if re.search(r'.*csv', f)]
+def format_deseq2(indir, outdir, db):
+    files = [f for f in os.listdir(os.path.realpath(indir))
+             if re.search(r'.*csv', f)]
 
     for f in files:
         f = os.path.join(indir, f)
@@ -70,9 +52,21 @@ def format_deseq2(indir, outdir):
             p_adj.append(c[5])
             genes.append(r)
 
-        make_rnk(genes, p_adj, f, outdir)
+        make_rnk(genes, p_adj, f, indir, outdir, db)
 
 
+def format_seurat(indir, outdir, db):
+    files = [f for f in os.listdir(os.path.realpath(indir))
+             if re.search(r'.*csv', f)]
 
+    for f in files:
+        f = os.path.join(indir, f)
+        logger.info("Processing {}".format(os.path.basename(f)))
+        df = pd.read_csv(os.path.realpath(f), header=0, index_col=0)
 
+        genes, p_adj = [], []
+        for r, c in df.iterrows():
+            p_adj.append(c[4])
+            genes.append(r)
 
+        make_rnk(genes, p_adj, f, indir, outdir, db)
