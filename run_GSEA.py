ca1ea9071f1be5567d17733d843f811c165cea15
Samuel Bunga <sambunga094@gmail.com>
2021-08-16 12:14:17 -0400

    Initial working version of the pipeline

--- run_GSEA.py
+++ run_GSEA.py
@@ -30,55 +30,18 @@ def mgi_to_hgnc_name(gene_list):
     return hgnc_name
 
 
-def make_rnk_files(files, DB):
-    
-    for f in files: 
-        file = pd.read_csv(os.path.join(WD, 'DEG_CLUSTER/', f), 
-                           header=0, index_col=0)
-        genes, p_adj = [], []
-        for r,c in file.iterrows():
-            try:
-                hgnc_symbol = mgi_to_hgnc_name([r])
-                if hgnc_symbol != None:
-                    genes.append(hgnc_symbol)
-                    p_adj.append(c[4])
-            except:
-                continue
-            
-        rnk_file = pd.DataFrame({'names': genes, 
-                                 'p_adj': p_adj})
-        f_name = str(re.search('[a-zA-Z0-9_]+', f).group())
-        rnk_file.to_csv(os.path.join(WD, 'rnk', f_name+'.rnk'),
-                       index=False,
-                       sep='\t',
-                       header=False)
-        # Run GSEA
-        try:
-            os.mkdir(os.path.join(WD, 'outputs', DB, f_name), exist_ok=True)
-        except:
-            pass
-        gseapy.prerank(os.path.join(WD, 'rnk', f_name+'.rnk'),
-                       os.path.join(WD, 'Database/c2.cp.kegg.v7.2.symbols.gmt'),
-                       os.path.join(WD, 'outputs', DB, f_name)
-                      )
+
 
 if __name__ == '__main__':
     args = parse_arguments()
 
-    INPUT = args.input
-    OUTPUT = args.output
+    INDIR = args.input
+    OUTDIR = args.output
     FORMAT = args.file_type
+    DB = args.database
 
-    if FORMAT == 'de':
-        format_deseq2("/Users/sbunga/gitHub/GSEApy/test_files/deseq2")
-    '''
-    try:
-        os.mkdir(os.path.join(WD, 'rnk'))
-    except FileExistsError:
-        pass
+    if FORMAT == 'd':
+        format_deseq2(INDIR, OUTDIR, DB)
 
-    _, _, files = next(os.walk(os.path.join(WD, 'DEG_CLUSTER')))
-
-
-    make_rnk_files(files, 'KEGG')
-    '''
+    elif FORMAT == 's':
+        format_seurat(INDIR, OUTDIR, DB)
